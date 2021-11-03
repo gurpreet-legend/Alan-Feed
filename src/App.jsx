9726e2fe7f2743b90581d32283c90bd15be3dc4f
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-04 01:24:17 +0530

    styles added to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards'
-
+import useStyles from './styles'
 
 const alanKey='f62a4482f030a60c59b23e8e8803491a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
 
     const [ newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
+    
     useEffect(() => {
         alanBtn({
             key: alanKey,
@@ -22,7 +24,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan App</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" alt="Alan logo" className={classes.alanLogo}/>
+            </div>
             <NewsCards articles={newsArticles}/>
         </div>
     )
