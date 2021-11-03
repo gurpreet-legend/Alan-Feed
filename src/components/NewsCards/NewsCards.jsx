91ecebeef3818529f5e9559ecc2cea53f369d8a4
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-04 01:15:51 +0530

    Frontpage cards adde

--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -3,10 +3,44 @@ import React from 'react'
 import NewsCard from '../NewsCard/NewsCard';
 import useStyles from './styles';
 
+const infoCards = [
+    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+];
+
 const NewsCards = ({articles}) => {
 
     const classes = useStyles();
-    
+    console.log(articles.length);
+    if(!articles.length){
+        return (
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+                            <div className={classes.card } style={{backgroundColor: infoCard.color}}>
+                                <Typography variant="h5">{infoCard.title}</Typography>
+                                {
+                                    infoCard.info 
+                                    ?  (<Typography variant="h6">
+                                            <strong>
+                                                {infoCard.title.split(' ')[2]} :
+                                            </strong>
+                                                <br />
+                                                {infoCard.info}
+                                        </Typography>) : null
+                                }
+                                <Typography variant="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        )
+    }
+
     return (
         <Grow in>
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -5,7 +5,23 @@ const styles = makeStyles({
         padding: '0 5%',
         width: '100%',
         margin: '0',
-    }
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: '45vh',
+        padding: '10%',
+        borderRadius: 10,
+        color: 'white',
+    },
+    infoCard: {
+        display: 'flex', 
+        flexDirection: 'column', 
+        textAlign: 'center',
+    },
 })
 
 export default styles;
