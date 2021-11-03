4d0a495fe09b3b69d30dd4849ad6f95f82488ee3
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-03 15:46:06 +0530

    Created style.js for NewsCards & added response breakpoints

--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -1,13 +1,22 @@
+import { Grid, Grow, Typography } from '@mui/material';
 import React from 'react'
 import NewsCard from '../NewsCard/NewsCard';
+import useStyles from './styles';
 
 const NewsCards = ({articles}) => {
+
+    const classes = useStyles();
+    
     return (
-        <div>
-            {articles.map((article, index) => (
-                <NewsCard key={index}/>
-            ))}
-        </div>
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {articles.map((article, index) => (
+                    <Grid key={index} item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+                        <NewsCard article={article} index={index}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </Grow>
     )
 }
 
---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+const styles = makeStyles({
+    container: {
+        padding: '0 5%',
+        width: '100%',
+        margin: '0',
+    }
+})
+
+export default styles;
