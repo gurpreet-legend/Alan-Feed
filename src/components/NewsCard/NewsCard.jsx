250f9c5a08562033078735f98778fb538a6c12bf
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-03 16:39:01 +0530

    NewsCards implemented succesfully

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, CardActionArea, Button, Typorgraphy, Typography} from '@mui/material' 
 
-const NewsCard = () => {
+import useStyles from './styles';
+
+const NewsCard = ( {article: {description, publishedAt, source, title, url, urlToImage}, index} ) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>NewsCard</h1>
-        </div>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia 
+                className={classes.media}
+                image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}
+                />
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutteButton variant="h5">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{index + 1}</Typography>
+            </CardActions>
+        </Card>
     )
 }
 
