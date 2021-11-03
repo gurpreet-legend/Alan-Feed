4aed4a24281ea2a03801424a2ac78c6cee65e828
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-03 14:57:29 +0530

    Alan AI set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+
+const alanKey='f62a4482f030a60c59b23e8e8803491a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand : ({command, articles}) => {
+                if(command === 'newHeadlines') {
+                    console.log(articles);
+                }
+            }
+        })
+    },[]);
     return (
         <div>
             <h1>Alan App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-import '.index.css';
+import './index.css';
 
 import App from './App';
 
