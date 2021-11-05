e3f5ee6ad738b6158a1b99fee5417832a2ba42f0
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-05 23:46:04 +0530

    finalizing

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards'
 import wordsToNumbers from 'words-to-numbers'
 import useStyles from './styles'
+require('dotenv').config();
 
-const alanKey='f62a4482f030a60c59b23e8e8803491a2e956eca572e1d8b807a3e2338fdd0dc/stage';
+const alanKey= process.env.REACT_APP_ALAN_KEY;
+// const alanKey= 'dec2bbd6c68b52fd59b23e8e8803491a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
 
