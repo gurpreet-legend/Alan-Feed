c71c4bd47e9eb5a091abebaa3b49e9aee317b8b4
Kailash Kejriwal <kailashkejriwal21@gmail.com>
2022-10-01 02:03:38 +0530

    shifted the data source

---
+++ src/components/data/infoCards.js
@@ -0,0 +1,9 @@
+
+const infoCards = [
+    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#1565c0', title: 'News by Categories/Countries', info: 'Business, Entertainment, General, Health, Science, Sports, Technology (from <Your_Country>)', text: 'Give me the latest Technology news from United States' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+];
+
+export default infoCards
