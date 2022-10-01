8c541dde74e79bdba0cacdca2f267c68b93a8809
Kailash Kejriwal <kailashkejriwal21@gmail.com>
2022-10-01 10:35:07 +0530

    Added ESLint conifg

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,47 +1,47 @@
 import React, { useState, useEffect, createRef } from 'react'
-import { Card, CardMedia, CardContent, CardActions, CardActionArea, Button, Typography} from '@mui/material' 
+import { Card, CardMedia, CardContent, CardActions, CardActionArea, Button, Typography } from '@mui/material'
 import classNames from 'classnames'
 import useStyles from './styles';
 
-const NewsCard = ( {article: {description, publishedAt, source, title, url, urlToImage}, index, activeArticle} ) => {
-    const classes = useStyles();
-    const [elRefs, setElRefs] = useState([]);
-    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50)
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, index, activeArticle }) => {
+  const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50)
 
-    //To create refs for each displayed card :
-    useEffect(()=> {
-        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
-    },[]);
+  //To create refs for each displayed card :
+  useEffect(() => {
+    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+  }, []);
 
-    //To scroll on the active article :
-    useEffect(() => {
-        if(index===activeArticle && elRefs[activeArticle]){
-            scrollToRef(elRefs[activeArticle]);
-        }
-    },[index, activeArticle, elRefs])
+  //To scroll on the active article :
+  useEffect(() => {
+    if (index === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle]);
+    }
+  }, [index, activeArticle, elRefs])
 
-    return (
-        <Card ref={elRefs[index]} className={classNames(classes.card, activeArticle === index ? classes.activeCard : null)} >
-            <CardActionArea href={url} target="_blank">
-                <CardMedia 
-                className={classes.media}
-                image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}
-                />
-                <div className={classes.details}>
-                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
-                </div>
-                <Typography className={classes.title} gutteButton variant="h5">{title}</Typography>
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary">Learn More</Button>
-                <Typography variant="h5" color="textSecondary">{index + 1}</Typography>
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card ref={elRefs[index]} className={classNames(classes.card, activeArticle === index ? classes.activeCard : null)} >
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+        </div>
+        <Typography className={classes.title} gutteButton variant="h5">{title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">Learn More</Button>
+        <Typography variant="h5" color="textSecondary">{index + 1}</Typography>
+      </CardActions>
+    </Card>
+  )
 }
 
 export default NewsCard
