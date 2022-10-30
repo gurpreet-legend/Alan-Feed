84c19a37be0dbeb0d6680ac64ac3a2ffa00b835f
Harshwardhan Patil <harshp3302@gmail.com>
2022-10-30 09:59:51 +0530

    added eslint-config-prettier package and formated the code

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'eslint:recommended'],
+  extends: ['plugin:react/recommended', 'eslint:recommended', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 12,
